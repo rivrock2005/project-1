185cbae7a1e6141060d81774dd9727b6700365ca
rivrock2005 <44785849+rivrock2005@users.noreply.github.com>
2019-02-14 14:03:57 -0800

    Add files via upload

--- my_cf_project 2/js/scripts.js
+++ my_cf_project 2/js/scripts.js
@@ -1,28 +1,47 @@
+
 $(document).ready(function(){
-  $(document).ready(function(){
-  // Smooth scrolling
-  var $root = $('html, body');
-  $('.navbar-nav a').click(function() {
-    var href = $.attr(this, 'href');
+// smooth scrolling
+var $root = $('html', 'body');
+$('#navbar-example a').click(function() {
+  var href = $.attr(this, 'href');
+  if (href != undefined && href != '#') {
     $root.animate({
       scrollTop: $(href).offset().top
     }, 500, function () {
       window.location.hash = href;
     });
-    return false;
-  });
+  }
+  return false;
+});
 
-  // Tooltips
-  $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
+// tooltips
+$(function () {
+  $('#suffolk').tooltip();
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
-  // textarea background
-  $(function () {
-    $("#item1").tooltip();
-  });
- </script>
+// textarea
+$(".message-box").css("border", "2px solid red");
+$(".message-box").css("background-color", "pink");
 
+// message form
+$('#button').on('click', function(){
+  var usrName = $('#name').val();
+  var usrEmail = $('#email').val();
+  var usrMsg = $('.message-box').val();
+  console.log('clicked');
+  // "store" Name
+  $('#msgName').html(usrName);
+  $('#name').hide();
 
+  // "store" email
+  $('#msgEmail').html(usrEmail);
+  $('#email').hide();
 
+  // "store" message
+  $('#msgBody').html(usrMsg);
+  $('.message-box').hide();
+
+  return false;
+});
 });
