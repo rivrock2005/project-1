56a34a1b6c89e9590431dc34db8c9e8dc82b401a
rivrock2005 <44785849+rivrock2005@users.noreply.github.com>
2019-02-19 09:21:33 -0800

    Add files via upload

--- my_cf_project 2/js/scripts.js
+++ my_cf_project 2/js/scripts.js
@@ -1,47 +1,46 @@
-
 $(document).ready(function(){
-// smooth scrolling
-var $root = $('html', 'body');
-$('#navbar-example a').click(function() {
-  var href = $.attr(this, 'href');
-  if (href != undefined && href != '#') {
-    $root.animate({
-      scrollTop: $(href).offset().top
-    }, 500, function () {
-      window.location.hash = href;
-    });
-  }
-  return false;
-});
+  // smooth scrolling
+  var $root = $('html', 'body');
+  $('#navbar-example a').click(function() {
+    var href = $.attr(this, 'href');
+    if (href != undefined && href != '#') {
+      $root.animate({
+        scrollTop: $(href).offset().top
+      }, 500, function () {
+        window.location.hash = href;
+      });
+    }
+    return false;
+  });
 
-// tooltips
-$(function () {
-  $('#suffolk').tooltip();
-  $('[data-toggle="tooltip"]').tooltip();
-});
+  // tooltips
+  $(function () {
+    $('#suffolk').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
+  });
 
-// textarea
-$(".message-box").css("border", "2px solid red");
-$(".message-box").css("background-color", "pink");
+  // textarea
+  $(".message-box").css("border", "2px solid red");
+  $(".message-box").css("background-color", "pink");
 
-// message form
-$('#button').on('click', function(){
-  var usrName = $('#name').val();
-  var usrEmail = $('#email').val();
-  var usrMsg = $('.message-box').val();
-  console.log('clicked');
-  // "store" Name
-  $('#msgName').html(usrName);
-  $('#name').hide();
+  // message form
+  $('#button').on('click', function(){
+    var usrName = $('#name').val();
+    var usrEmail = $('#email').val();
+    var usrMsg = $('.message-box').val();
+    console.log('clicked');
+    // "store" Name
+    $('#msgName').html(usrName);
+    $('#name').hide();
 
-  // "store" email
-  $('#msgEmail').html(usrEmail);
-  $('#email').hide();
+    // "store" email
+    $('#msgEmail').html(usrEmail);
+    $('#email').hide();
 
-  // "store" message
-  $('#msgBody').html(usrMsg);
-  $('.message-box').hide();
+    // "store" message
+    $('#msgBody').html(usrMsg);
+    $('.message-box').hide();
 
-  return false;
-});
+    return false;
+  });
 });
