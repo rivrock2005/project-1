f4be0a10469b179b7e4dd363d66e180dd37983b0
rivrock2005 <44785849+rivrock2005@users.noreply.github.com>
2019-03-06 09:55:46 -0800

    Add files via upload

---
+++ my_cf_project 2/my_cf_project 2/js/scripts.js
@@ -0,0 +1,116 @@
+// Facebook Code
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : 'your-app-id',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.0'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
+$(document).ready(function(){
+   // your code goes here
+   // Smooth Scrolling
+     var $root = $('html, body');
+     $('#navbar-example a').click(function(event) {
+       var href = $.attr(this, 'href');
+       if (href != undefined && href != '#') {
+         $root.animate({
+           scrollTop: $(href).offset().top
+         }, 500, function () {
+           window.location.hash = href;
+         });
+       }
+       event.preventDefault();
+     });
+
+   // Tooltips
+     $ (function () {
+       $('resume').tooltip();
+     });
+
+     $(function () {
+       $('[data-toggle="tooltip"]').tooltip();
+     });
+
+     // Textarea background color
+     $(".message-box").css("background-color", "#e6fff4");
+
+     //Message box character count
+     $(".message-box").on("keyup", function() {
+         var charCount = $(".message-box").val().length; //here we set the length of the content of the textarea to a variable// in here is where the rest of our code for this Exercise will go
+         console.log("keyup happened");
+          $("#char-count").html(charCount); // here we show a running character count to the user
+          if(charCount > 100) {
+            $('#char-count').css('color', 'red');
+          } else {
+            $('#char-count').css('color', 'black');
+          };
+     });
+
+     // Contact Submission
+     $('#button').on('click', function() {
+         var comment = $('.message-box').val();
+         var prefix = "Thank you, "
+         var suffix = ". We have received your message!"
+         var name = $('.text-box1').val();
+         var email = $('.email-box').val()
+
+         if (comment === "") {
+             console.log("message not caputred");
+             $(".message-box").css("border", "2px solid red");
+         } else {
+               $(".message-box").css("border", "");
+         } if (name === "") {
+             $(".text-box1").css("border", "2px solid red");
+         } else {
+             $(".text-box1").css("border", "");
+         } if (email === "") {
+             $(".email-box").css("border", "2px solid red");
+         } else {
+             $(".email-box").css("border", "");
+             $('.text-box1').hide();
+             $('.text-box2').hide();
+             $('.phone-box').hide();
+             $('.email-box').hide();
+             $('.message-box').hide(); // maybe keep the message box to show the viewer their message?
+             $('#char-count').hide();
+             $('#visible-salutation').html(prefix+name+suffix);
+             $('#visible-comment').html(comment);
+        }
+         return false;
+     });
+
+     //images /work section
+     // console.log(images); to test the connection count of images
+     for (var i = 0; i < works.length; ++i) {
+        $("#work-images").append("\
+           <div class='col-sm-12 col-md-6 col-lg-6'>\
+           <a href='" + works[i].url + "' target='_blank' class='work-img'>\
+             <img class='img-fluid' src='" + works[i].pic + "'>\
+             <span class='info'><p class='proj-title'>Title:</p>" + works[i].title + "</span>\
+             </a>\
+           </div>\
+         ");
+         var images = $("#work-images img");
+       if (i % 2 === 0) {
+           $(images[i]).css("border", "1px solid #0e8 ");
+       } else {
+           $(images[i]).css("border", "1px solid black");
+       };
+   };
+   $(".work-img").mouseenter(function(){
+     $(".info", this).show().css("text-shadow", "2px 2px 5px black");
+   }).mouseleave(function(){
+     $(".info", this).hide();
+   });
+});
---
+++ my_cf_project 2/my_cf_project 2/js/works.js
@@ -0,0 +1,42 @@
+var works =[
+  {
+    title: "First Project",
+    pic: "img/download.png",
+    url: "https://www.slideshare.net/slideshow/embed_code/key/tIUFsUCQAM0otY"
+  },
+  {
+    title: "Second Project",
+    pic: "img/download.png",
+    url: "https://www.krop.com/profile/mjennings/"
+  },
+  {
+    title: "Third Project",
+    pic: "img/download.png",
+    url: "https://www.slideshare.net/slideshow/embed_code/key/tIUFsUCQAM0otY"
+  },
+  {
+    title: "Fourth Project",
+    pic: "img/download.png",
+    url: "https://www.krop.com/profile/mjennings/"
+  },
+  {
+    title: "Fifth Project",
+    pic: "img/me.jpeg",
+    url: "https://www.slideshare.net/slideshow/embed_code/key/tIUFsUCQAM0otY"
+  },
+  {
+    title: "Sixth Project",
+    pic: "img/ocean.jpg",
+    url: "https://www.krop.com/profile/mjennings/"
+  },
+  {
+    title: "Seventh Project",
+    pic: "img/beach.jpg",
+    url: "https://www.slideshare.net/slideshow/embed_code/key/tIUFsUCQAM0otY"
+  },
+  {
+    title: "Eighth Project",
+    pic: "img/ocean.jpg",
+    url: "https://www.krop.com/profile/mjennings/"
+  }
+]
